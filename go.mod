ef9aafcb735b4b78d2d0786f3725b6ee10f86819
andrew beatrice <abeatrice.mail@gmail.com>
2021-05-04 16:40:13 -0700

    add bestbuy

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module app
 go 1.16
 
 require (
+	github.com/EDDYCJY/fake-useragent v0.2.0 // indirect
+	github.com/PuerkitoBio/goquery v1.6.1 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/mailgun/mailgun-go/v4 v4.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
+	browser "github.com/EDDYCJY/fake-useragent"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/mailgun/mailgun-go/v4"
 )
@@ -28,6 +30,39 @@ type AvilableToPromiseNetwork struct {
 }
 
 func main() {
+	checkBestBuy()
+	checkTarget()
+	fmt.Printf("[%s] Done Checking PS5 Stock\n", time.Now().Format("2006-01-02 15:04:05"))
+}
+
+func checkBestBuy() {
+	url := "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", browser.Chrome())
+	res, err := client.Do(req)
+	check(err)
+
+	body, err := ioutil.ReadAll(res.Body)
+	check(err)
+
+	if strings.Contains(string(body), "Add to Cart") {
+		mg := mailgun.NewMailgun("sandbox7098dd5d68634781a39da81e50dfa7de.mailgun.org", os.Getenv("MAILGUN_KEY"))
+		message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", "PS5 stock avilable at bestbuy https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149", "abeatrice.mail@gmail.com")
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		_, _, err := mg.Send(ctx, message)
+		check(err)
+
+		fmt.Println("ps5 avilable at bestbuy")
+	}
+
+	res.Body.Close()
+}
+
+func checkTarget() {
 	// target ps5 product id 81114595
 	// junk 80208042
 	res, err := http.Get("https://redsky.target.com/v3/pdp/tcin/81114595?excludes=awesome_shop,question_answer_statistics,item,taxonomy,bulk_ship,rating_and_review_reviews,rating_and_review_statistics&key=eb2551e4accc14f38cc42d32fbc2b2ea")
@@ -42,7 +77,7 @@ func main() {
 
 	if targetPs5.Product.AvilableToPromiseNetwork.AvilableToPromiseQuantity > 0 {
 		mg := mailgun.NewMailgun("sandbox7098dd5d68634781a39da81e50dfa7de.mailgun.org", os.Getenv("MAILGUN_KEY"))
-		message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", "PS5 stock avilable at target", "abeatrice.mail@gmail.com")
+		message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", "PS5 stock avilable at target: https://www.target.com/p/playstation-5-console/-/A-81114595", "abeatrice.mail@gmail.com")
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
 
@@ -53,8 +88,6 @@ func main() {
 	}
 
 	res.Body.Close()
-
-	fmt.Println("DONE")
 }
 
 func check(err error) {
