0be42f1a6f947a06a21c1ddece44a10b73959ec5
andrew beatrice <abeatrice.mail@gmail.com>
2021-05-07 14:45:20 -0700

    add walmart

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	browser "github.com/EDDYCJY/fake-useragent"
+	"github.com/PuerkitoBio/goquery"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/mailgun/mailgun-go/v4"
 )
@@ -31,10 +32,13 @@ type AvilableToPromiseNetwork struct {
 }
 
 var wg sync.WaitGroup
+var client = &http.Client{}
+var userAgent string = browser.Chrome()
 
 func main() {
-	wg.Add(2)
+	wg.Add(3)
 
+	go checkWalmart()
 	go checkBestBuy()
 	go checkTarget()
 
@@ -42,42 +46,71 @@ func main() {
 	fmt.Printf("[%s] Done Checking PS5 Stock\n", time.Now().Format("2006-01-02 15:04:05"))
 }
 
+func get(url string) *http.Response {
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", userAgent)
+	res, err := client.Do(req)
+	check(err)
+	return res
+}
+
+func email(body string) {
+	mg := mailgun.NewMailgun("sandbox7098dd5d68634781a39da81e50dfa7de.mailgun.org", os.Getenv("MAILGUN_KEY"))
+	message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", body, "abeatrice.mail@gmail.com")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	_, _, err := mg.Send(ctx, message)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkWalmart() {
+	defer wg.Done()
+
+	url := "https://www.walmart.com/ip/Sony-PlayStation-5/363472942"
+	res := get(url)
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	check(err)
+
+	doc.Find(".prod-ProductCTA").Each(func(i int, s *goquery.Selection) {
+		if s.Find(".button-wrapper").Text() == "Add to cart" {
+			email(fmt.Sprintf("ps5 avilable at walmart: %s", url))
+			fmt.Println("ps5 avilable at walmart")
+		}
+	})
+}
+
 func checkBestBuy() {
 	defer wg.Done()
 
 	url := "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("User-Agent", browser.Chrome())
-	res, err := client.Do(req)
-	check(err)
+	res := get(url)
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	check(err)
 
 	if strings.Contains(string(body), "Add to Cart") {
-		mg := mailgun.NewMailgun("sandbox7098dd5d68634781a39da81e50dfa7de.mailgun.org", os.Getenv("MAILGUN_KEY"))
-		message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", "PS5 stock avilable at bestbuy https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149", "abeatrice.mail@gmail.com")
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer cancel()
-
-		_, _, err := mg.Send(ctx, message)
-		check(err)
-
+		email("PS5 stock avilable at bestbuy https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
 		fmt.Println("ps5 avilable at bestbuy")
 	}
-
-	res.Body.Close()
 }
 
 func checkTarget() {
 	defer wg.Done()
 
 	// target ps5 product id 81114595
-	// junk 80208042
 	res, err := http.Get("https://redsky.target.com/v3/pdp/tcin/81114595?excludes=awesome_shop,question_answer_statistics,item,taxonomy,bulk_ship,rating_and_review_reviews,rating_and_review_statistics&key=eb2551e4accc14f38cc42d32fbc2b2ea")
 	check(err)
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	check(err)
@@ -87,22 +120,7 @@ func checkTarget() {
 	check(err)
 
 	if targetPs5.Product.AvilableToPromiseNetwork.AvilableToPromiseQuantity > 0 {
-		mg := mailgun.NewMailgun("sandbox7098dd5d68634781a39da81e50dfa7de.mailgun.org", os.Getenv("MAILGUN_KEY"))
-		message := mg.NewMessage("abeatrice.mail@gmail.com", "PS5 Avilable!", "PS5 stock avilable at target: https://www.target.com/p/playstation-5-console/-/A-81114595", "abeatrice.mail@gmail.com")
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer cancel()
-
-		_, _, err := mg.Send(ctx, message)
-		check(err)
-
+		email("PS5 stock avilable at target: https://www.target.com/p/playstation-5-console/-/A-81114595")
 		fmt.Println("ps5 avilable at target")
-	}
-
-	res.Body.Close()
-}
-
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
 	}
 }
